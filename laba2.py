560507896a49093407856c89f260c136e5a32b5f
IT-STUDENT <it-student@MEPHI3.LESNOY>
2018-09-13 14:12:53 +0300

    Dima

---
+++ laba2.py
@@ -0,0 +1,49 @@
+import turtle
+x1=0
+b1=20
+turtle.position()
+
+
+
+
+
+turtle.shape("turtle")
+turtle.penup()
+turtle.goto(-250, -250)
+turtle.pendown()
+
+
+turtle.forward(50)
+turtle.right(-90)
+turtle.forward(50)
+turtle.right(-90)
+turtle.forward(50)
+turtle.right(90)
+turtle.forward(50)
+turtle.right(90)
+turtle.forward(50)
+
+
+
+turtle.penup()
+turtle.home()
+turtle.pendown()
+x=0
+b=20
+while x<40:
+    turtle.forward(b)
+    turtle.right(-90)
+    x+=1
+    b+=5
+ 
+turtle.penup()
+turtle.goto(250, 250)
+turtle.pendown()
+
+while x1<30:
+    turtle.circle(b1, 180)
+    x1+=1
+    b1=b1+5
+
+
+print ("End")    
---
+++ laba2_2.py
@@ -0,0 +1,30 @@
+import turtle
+
+
+turtle.position()
+
+turtle.circle(20, 180)
+turtle.circle(20, 180)
+
+
+turtle.penup()
+turtle.goto(-250, -250)
+turtle.pendown()
+c=10
+b=-250
+x=0
+while x<10:
+    turtle.forward(c)
+    turtle.right(-90)
+    turtle.forward(c)
+    turtle.right(-90)
+    turtle.forward(c)
+    turtle.right(-90)
+    turtle.forward(c)
+    turtle.right(-90)
+    c+=20
+    turtle.penup()
+    b-=10
+    turtle.goto(b, b)
+    turtle.pendown()
+    x+=1
